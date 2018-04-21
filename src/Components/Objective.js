85db9e2c733bb8ef4cc51f6d7318cba5bc7ddc39
Raul Lopez Luna <raullopez2094@gmail.com>
2018-04-21 16:10:49 -0700

    update colors

--- src/Components/Objective.js
+++ src/Components/Objective.js
@@ -10,18 +10,28 @@ export default function Objective(){
         </p>
 
         <div className="content">
-          <p >I am looking for my first full time software development job. I have
+          <p>
+            I am a recent graduate looking for my first full time software
+            development job. I have a strong background in CS and can quickly
+            learn new tools, frameworks, and languages.
+          </p>
+          <p>
+            As a software developer, I am looking for jobs under Web Development
+            and Quality Assurance. My biggest interests lie in Front End but I
+            am excited to learn about other roles and possibilities.
+          </p>
+          {/* <p >I am looking for my first full time software development job. I have
             experience working with multiple parts of the javascript web dev stack
             using Node.js and compatible npm repos. Angular, React, and Vue are very
             powerful frameworks that I have spent a lot of time with. I have worked with
             mongodb and postgresql.
-          </p>
-          <p>
+            </p>
+            <p>
             I love working in small teams, but am also very independent
             and value autonomy. I have participated in hackathons and project based
             classes where team organzation and colaboration were critical in order
             to meet deadlines and complete sprints.
-          </p>
+          </p> */}
         </div>
       </div>
     </div>
--- src/Components/Work.js
+++ src/Components/Work.js
@@ -63,40 +63,15 @@ export default class Work extends Component{
           <a href={this.getHref()}><button onClick={this.handleClick}>{this.buttonLabel()}</button></a>
           <div className={this.showOrHide()}>
             <div className="WriteupText">
-              <p>I worked at Leeps Lab @ UCSC under Economics Professor Kristian Lopez Vargas
-                the summer after my junior year.
-                I collaborated with the lab staff to design and organize web surveys that changed based on
-                the responses to previous questions. I took over an existing Angular project, where two participants anonymous
-                to each other decided through digital communication how to distribute real money earned through the surveys.
-                My contribution was adding features to this project based on the designs and interactions we expected from
-                the participants. This included css styling, new pages with new questions and interactions, and redesigning the
-                flow of the interaction. The data was controlled by RedWood, a framework designed for this type of real time interaction,
-                where different app pages were presented dinamically. RedWood and Angular paired together well, and the majority of the app
-                was coded in JS and ran as a single page application. Data was handled by Angular $scope, pushed to RedWood, and updated as needed after each app page was completed.
+              <p>I worked at Leeps econ research lab under Kristian Lopez Vargas the summer after my junior year. The goal of my project was to develop web powered behavioral experiments that changed based on the responses to previous questions. I took over an existing Angular project, where two anonymous participants decided how to distribute real money earned through their interactions in the experiments. The data was controlled by RedWood, a framework designed for synchronizing data across participants from real time interaction. RedWood and Angular paired together well, and the majority of the app was coded in JS and ran as a single page application. Data was handled by Angular $scope, pushed to RedWood, and updated as needed after each app page was completed. It was the first time I worked on a web project as large as this. My contributions include adding features to this project based on the designs by lab staff. This included css styling, new pages with new interactions, and redesigning the flow of the interactions.
               </p>
 
-              <p>Once the project was ready to be used by participants, I was tasked with recreating the project in another framework.
-                Otree was powered by python and Django, and was perfect for the lab's social experiements. I was the first in the lab to
-                create a project with Otree, which met all our expectations. The project needed a major redesign since Otree
-                encouraged a more linear approach to the flow of the app pages. The experiment data was now managed by a postgresql
-                database. Despite the differences, development was faster with oTree, and its compatibility with PyCharm IDE made debugging easier,
-                since the database was always visible.
+              <p>Once the Redwood and Angular project was ready to be used by participants, I was tasked with recreating the project in another framework. Otree was powered by Python and Django, and was perfect for the lab's behavioral experiments. I was the first in the lab to create a project with Otree. Its functionality met all our requirements. The project needed a major redesign since Otree encouraged a more linear approach to the flow of the app pages. Despite the differences, development was faster with oTree, and its compatibility with PyCharm IDE made debugging easier, since the database was always visible.
+
               </p>
 
-              <p>I learned a lot about clear communication and the importance of design before
-                any coding is done. Kristian and the other staff did not have a very technical
-                background, so clear concise communication was required on both ends. I created multiple README pages that could be understood
-                by non-programmers
-                explaining how to clone, run, and test the project in his local computer. The documentation also served as a
-                primer to Otree. In addition,
-                I learned how to effectively use github for version control. Having separate production and
-                development branches allowed me to become familiar with the code base without the fear of
-                disrupting live code. I worked remotely from Oakland, and met with Kristian once a week through skype
-                and once a week in the lab. Lab meetings usually resulted in a whiteboard full of digrams, flow charts for wireframes,
-                multiple mockups for different components, and deadlines set by priority. It was important to know what questions to ask, ideas to propose, and
-                progress to share ahead of time.
-                I enjoyed the autonomy and responsibility, and ultimately how I
-                approached the project was up to me.
+              <p>I learned to communicate clearly with the staff and to complete design before any coding is done. The lab staff did not have a very technical background, so clear concise communication was required on both ends. I created README pages that could be understood by non-programmers explaining how to clone, run, and test the project. The documentation also served as a primer to Otree. In addition, I learned how to effectively use github for version control. Having separate production and development branches allowed me to become familiar with the code base without the fear of disrupting live code. When I wasn’t in the lab, I worked remotely from Oakland. Regardless of if a meeting took place through skype or in the lab, meetings usually resulted in a whiteboard full of diagrams, flow charts, wireframe mockups for different components, and deadlines set by priority. It was important to know what questions to ask, ideas to propose, and progress to share ahead of time. I enjoyed the autonomy and responsibility while working with Leeps.
+                
               </p>
             </div>
             <a href={this.getHref()}><button onClick={this.handleClick}>{this.buttonLabel()}</button></a>
